3e56ca8ef0bf959d56c8d9ff4afc75a6ca997bfd
Peter Njoroge <petenjorom@gmail.com>
2021-10-11 15:06:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,474 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager,Screen
+from kivy.uix.pagelayout import PageLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
+
+class MainWindow(Widget):
+    pass
+
+class Screen1(Screen):
+    pass
+
+class Screen2(Screen):
+    pass
+
+class Screen_Peter(Screen):
+    pass
+
+class Screenmanagement(ScreenManager):
+    pass
+
+
+class Screen_Wrong(Screen):
+    pass
+class Wrong1(BoxLayout):
+    pass
+
+class Wrong2(BoxLayout):
+    pass
+
+
+
+
+
+class Pagesonfirst(PageLayout):
+    pass
+
+class Boxlayoutexample1(BoxLayout):
+    pass
+
+class Boxlayoutexample2(BoxLayout):
+    pass
+
+class Boxlayoutexample3(BoxLayout):
+    pass
+
+class Boxlayoutexample4(BoxLayout):
+    pass
+
+class Boxlayoutexample5(BoxLayout):
+    pass
+
+class Boxlayoutexample6(BoxLayout):
+    pass
+
+class Boxlayoutexample7(BoxLayout):
+    pass
+
+class Boxlayoutexample8(BoxLayout):
+    pass
+
+class Boxlayoutexample9(BoxLayout):
+    pass
+
+class Boxlayoutexample10(BoxLayout):
+    pass
+
+class Boxlayoutexample11(BoxLayout):
+    pass
+
+class Boxlayoutexample12(BoxLayout):
+    pass
+
+
+class Scroller(ScrollView):
+    pass
+
+class PagesPeter(PageLayout):
+    pass
+
+
+
+
+
+class Screen_PAMELA(Screen):
+    pass
+class PAMELA1(BoxLayout):
+    pass
+class PAMELA2(BoxLayout):
+    pass
+class PAMELA3(BoxLayout):
+    pass
+class PAMELA4(BoxLayout):
+    pass
+class PAMELA5(BoxLayout):
+    pass
+class PagesPAMELA(PageLayout):
+    pass
+
+
+
+class Screen_DOREEN(Screen):
+    pass
+class DOREEN1(BoxLayout):
+    pass
+class DOREEN2(BoxLayout):
+    pass
+class DOREEN3(BoxLayout):
+    pass
+class DOREEN4(BoxLayout):
+    pass
+class DOREEN5(BoxLayout):
+    pass
+class PagesDOREEN(PageLayout):
+    pass
+
+
+
+
+
+class Screen_MIKE(Screen):
+    pass
+class MIKE1(BoxLayout):
+    pass
+class MIKE2(BoxLayout):
+    pass
+class MIKE3(BoxLayout):
+    pass
+class MIKE4(BoxLayout):
+    pass
+class MIKE5(BoxLayout):
+    pass
+class PagesMIKE(PageLayout):
+    pass
+
+
+
+
+
+
+class Screen_JOHN(Screen):
+    pass
+class JOHN1(BoxLayout):
+    pass
+class JOHN2(BoxLayout):
+    pass
+class JOHN3(BoxLayout):
+    pass
+class JOHN4(BoxLayout):
+    pass
+class JOHN5(BoxLayout):
+    pass
+class PagesJOHN(PageLayout):
+    pass
+
+
+
+
+
+class Screen_CENTRINE(Screen):
+    pass
+class CENTRINE1(BoxLayout):
+    pass
+class CENTRINE2(BoxLayout):
+    pass
+class CENTRINE3(BoxLayout):
+    pass
+class CENTRINE4(BoxLayout):
+    pass
+class CENTRINE5(BoxLayout):
+    pass
+class PagesCENTRINE(PageLayout):
+    pass
+
+
+
+
+
+
+class Screen_JOSEPH(Screen):
+    pass
+class JOSEPH1(BoxLayout):
+    pass
+class JOSEPH2(BoxLayout):
+    pass
+class JOSEPH3(BoxLayout):
+    pass
+class JOSEPH4(BoxLayout):
+    pass
+class JOSEPH5(BoxLayout):
+    pass
+class PagesJOSEPH(PageLayout):
+    pass
+
+
+
+
+
+
+
+
+class Screen_BENARD(Screen):
+    pass
+class BENARD1(BoxLayout):
+    pass
+class BENARD2(BoxLayout):
+    pass
+class BENARD3(BoxLayout):
+    pass
+class BENARD4(BoxLayout):
+    pass
+class BENARD5(BoxLayout):
+    pass
+class PagesBENARD(PageLayout):
+    pass
+
+
+
+
+class Screen_BENSON(Screen):
+    pass
+class BENSON1(BoxLayout):
+    pass
+class BENSON2(BoxLayout):
+    pass
+class BENSON3(BoxLayout):
+    pass
+class BENSON4(BoxLayout):
+    pass
+class BENSON5(BoxLayout):
+    pass
+class PagesBENSON(PageLayout):
+    pass
+
+
+
+
+class Screen_CLINTON(Screen):
+    pass
+class CLINTON1(BoxLayout):
+    pass
+class CLINTON2(BoxLayout):
+    pass
+class CLINTON3(BoxLayout):
+    pass
+class CLINTON4(BoxLayout):
+    pass
+class CLINTON5(BoxLayout):
+    pass
+class PagesCLINTON(PageLayout):
+    pass
+
+
+
+class Screen_SAMUEL(Screen):
+    pass
+class SAMUEL1(BoxLayout):
+    pass
+class SAMUEL2(BoxLayout):
+    pass
+class SAMUEL3(BoxLayout):
+    pass
+class SAMUEL4(BoxLayout):
+    pass
+class SAMUEL5(BoxLayout):
+    pass
+class PagesSAMUEL(PageLayout):
+    pass
+
+
+
+class Screen_LOISE(Screen):
+    pass
+class LOISE1(BoxLayout):
+    pass
+class LOISE2(BoxLayout):
+    pass
+class LOISE3(BoxLayout):
+    pass
+class LOISE4(BoxLayout):
+    pass
+class LOISE5(BoxLayout):
+    pass
+class PagesLOISE(PageLayout):
+    pass
+
+
+
+class Screen_BRIAN(Screen):
+    pass
+class BRIAN1(BoxLayout):
+    pass
+class BRIAN2(BoxLayout):
+    pass
+class BRIAN3(BoxLayout):
+    pass
+class BRIAN4(BoxLayout):
+    pass
+class BRIAN5(BoxLayout):
+    pass
+class PagesBRIAN(PageLayout):
+    pass
+
+
+
+
+class Screen_COLLINS(Screen):
+    pass
+class COLLINS1(BoxLayout):
+    pass
+class COLLINS2(BoxLayout):
+    pass
+class COLLINS3(BoxLayout):
+    pass
+class COLLINS4(BoxLayout):
+    pass
+class COLLINS5(BoxLayout):
+    pass
+class PagesCOLLINS(PageLayout):
+    pass
+
+
+
+
+
+class Screen_ERIC(Screen):
+    pass
+class ERIC1(BoxLayout):
+    pass
+class ERIC2(BoxLayout):
+    pass
+class ERIC3(BoxLayout):
+    pass
+class ERIC4(BoxLayout):
+    pass
+class ERIC5(BoxLayout):
+    pass
+class PagesERIC(PageLayout):
+    pass
+
+
+
+
+
+
+class Screen_JOHNM(Screen):
+    pass
+class JOHNM1(BoxLayout):
+    pass
+class JOHNM2(BoxLayout):
+    pass
+class JOHNM3(BoxLayout):
+    pass
+class JOHNM4(BoxLayout):
+    pass
+class JOHNM5(BoxLayout):
+    pass
+class PagesJOHNM(PageLayout):
+    pass
+
+
+
+
+
+
+
+class Screen_RUTH(Screen):
+    pass
+class RUTH1(BoxLayout):
+    pass
+class RUTH2(BoxLayout):
+    pass
+class RUTH3(BoxLayout):
+    pass
+class RUTH4(BoxLayout):
+    pass
+class RUTH5(BoxLayout):
+    pass
+class PagesRUTH(PageLayout):
+    pass
+
+
+
+class Screen_RUTHM(Screen):
+    pass
+class RUTHM1(BoxLayout):
+    pass
+class RUTHM2(BoxLayout):
+    pass
+class RUTHM3(BoxLayout):
+    pass
+class RUTHM4(BoxLayout):
+    pass
+class RUTHM5(BoxLayout):
+    pass
+class PagesRUTHM(PageLayout):
+    pass
+
+
+
+class Screen_RONALD(Screen):
+    pass
+class RONALD1(BoxLayout):
+    pass
+class RONALD2(BoxLayout):
+    pass
+class RONALD3(BoxLayout):
+    pass
+class RONALD4(BoxLayout):
+    pass
+class RONALD5(BoxLayout):
+    pass
+class PagesRONALD(PageLayout):
+    pass
+
+
+
+
+class Screen_NICHOLAS(Screen):
+    pass
+class NICHOLAS1(BoxLayout):
+    pass
+class NICHOLAS2(BoxLayout):
+    pass
+class NICHOLAS3(BoxLayout):
+    pass
+class NICHOLAS4(BoxLayout):
+    pass
+class NICHOLAS5(BoxLayout):
+    pass
+class PagesNICHOLAS(PageLayout):
+    pass
+
+
+
+
+class Screen_Whole(Screen):
+    pass
+class Whole1(BoxLayout):
+    pass
+class Whole2(BoxLayout):
+    pass
+class Whole3(BoxLayout):
+    pass
+class Whole4(BoxLayout):
+    pass
+class Whole5(BoxLayout):
+    pass
+class PagesWhole(PageLayout):
+    pass
+
+
+
+class Screen_About(Screen):
+    pass
+class About1(BoxLayout):
+    pass
+class About2(BoxLayout):
+    pass
+class About3(BoxLayout):
+    pass
+class About4(BoxLayout):
+    pass
+
+class MainApp(App):
+    def Button1press(self):
+        passw = "BACKSPACE"
+        inp=self.root.ids["user_pass"].text.upper()
+        if inp==passw:
+            self.root.current="Screen2"
+        else:
+            self.root.current="Screen_Wrong"
+MainApp().run()
